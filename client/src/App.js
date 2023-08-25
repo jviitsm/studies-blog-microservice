563204b9043b708d86a85d89a4490f5ca0324acf
joao modesto <jvictorgmodesto@gmail.com>
2023-08-25 16:33:39 -0300

    comments implementado

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import PostCreate from "./Components/PostCreate";
+import PostCreate from "./Components/Posts/PostCreate";
+import PostList from "./Components/Posts/PostList";
 
 export default function App() {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
