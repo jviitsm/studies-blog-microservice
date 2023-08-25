4361adb109b5e0a93ddc3414a1c144f88c5aac01
joao modesto <jvictorgmodesto@gmail.com>
2023-08-24 21:59:02 -0300

    rotas get e post

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+const port = 4000;
+
+const posts = {};
+
+app.get("/posts", (req, res) => res.send(posts));
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
