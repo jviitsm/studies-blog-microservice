155d8f62a694ff04df5f5a032a52e5855ebf1737
joao modesto <jvictorgmodesto@gmail.com>
2023-08-25 23:50:17 -0300

    ajustado query service

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParsers = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParsers.json());
